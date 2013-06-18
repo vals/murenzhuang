9ad9214f5e630d480e963abdff69cab47aa3a9fb
Valentine Svensson <valentine@nxn.se>
2013-06-18 20:43:30 +0200

    First MRZ session

--- skeletons/python/std.py
+++ skeletons/python/std.py
@@ -1,3 +1,77 @@
+""" MRZ Main Module
+"""
 
-def input(str):
-    print str
+
+def copy(x_i):
+    raise NotImplemented
+
+
+def decrease_char(x_i):
+    decrease_map = {"a": "z", "b": "a", "c": "b", "d": "c", "e": "d", "f": "e",
+    "g": "f", "h": "g", "i": "h", "j": "i", "k": "j", "l": "k", "m": "l", "n":
+    "m", "o": "n", "p": "o", "q": "p", "r": "q", "s": "r", "t": "s", "u": "t",
+    "v": "u", "w": "v", "x": "w", "y": "x", "z": "y"}
+    return decrease_map[x_i]
+
+
+def decrease(x_i):
+    try:
+        x_i = int(x_i)
+        return x_i - 1 % 10
+    except ValueError:
+        return decrease_char(x_i)
+
+
+class Mutator(object):
+    def __init__(self, buffer_size, transform, transform_idx, target):
+        self.buffer_size = buffer_size
+        self.buffer = []
+
+        self.transform = transform
+        self.transform_idx = transform_idx
+
+        self.target = target
+
+    def input(self, x):
+        self.buffer.append(x)
+        if len(self.buffer) == self.buffer_size:
+            for i in self.transform_idx:
+                self.target.input(self.transform(self.buffer[i - 1]))
+            self.buffer = []
+
+    def done(self):
+        self.target.done()
+
+
+class Printer(object):
+    def __init__(self):
+        self.output = []
+
+    def input(self, x):
+        self.output.append(x)
+
+    def done(self):
+        print ''.join(self.output)
+        self.output = []
+
+
+class FilePrinter(Printer):
+    def __init__(self, file_name):
+        super(FilePrinter, self).__init__()
+        self.file_name = file_name
+
+    def done(self):
+        with open(self.file_name, 'w') as fh:
+            fh.write(''.join(self.output))
+
+
+def input(inp_str, case=0):
+    printer = Printer()
+    cases = [printer]
+    cases.append(Mutator(3, decrease, [2], printer))
+    cases.append(Mutator(3, decrease, [2], FilePrinter('file_11')))
+    cases.append(Mutator(4, copy, [1], printer))
+    for x in inp_str:
+        cases[case].input(x)
+
+    cases[case].done()
--- skeletons/python/tests.py
+++ skeletons/python/tests.py
@@ -3,19 +3,56 @@ import sys
 import os
 import std
 
-class MyTest(unittest.TestCase):
-    
+
+class TestRoot(unittest.TestCase):
+
     def test_root_program(self):
         sys.stdout = open('std_out_result', 'w')
         std.input("hello world")
         sys.stdout.close()
-        self.assertEqual( in_stdout(), "hello world\n")
-        
+        self.assertEqual(in_stdout(), "hello world\n")
+
     def tearDown(self):
         os.remove('std_out_result')
 
+
+class TestCase1(unittest.TestCase):
+
+    def test_case_1(self):
+        sys.stdout = open('std_out_result', 'w')
+        std.input("hello23world", 1)
+        sys.stdout.close()
+        self.assertEqual(in_stdout(), "dnvk\n")
+
+    def tearDown(self):
+        os.remove('std_out_result')
+
+
+class TestCase2(unittest.TestCase):
+
+    def test_case_2(self):
+        std.input("hello23world", 2)
+        with open('file_11', 'r') as fh:
+            output = fh.read()
+
+        self.assertEqual(output, "dnvk")
+
+
+class TestCase3(unittest.TestCase):
+
+    def test_case_3(self):
+        sys.stdout = open('std_out_result', 'w')
+        std.input("hello23world", 3)
+        sys.stdout.close()
+        self.assertEqual(in_stdout(), "hoo\n")
+
+    def tearDown(self):
+        os.remove('std_out_result')
+
+
 def in_stdout():
     return open('std_out_result', 'r').read()
+
 
 def main():
     unittest.main()
